9cb2f3397136eae2395d9ee57a44524444044a9d
whykatherine <whykatherine@gmail.com>
2019-11-02 13:07:32 -0700

    feat: allow for BC/BCE and validate input

--- index.js
+++ index.js
@@ -3,23 +3,38 @@ var cheerio = require('cheerio')
 var fs = require('fs')
 var prompt = require('prompt')
 
-// set up variables for user input in command line
-var prompt_attributes = [
-  {
-    name: 'year'
+// set up command line prompt for user input
+var schema = {
+  properties: {
+    year: {
+      description: "Enter a non-zero year (postive for AD/CE, negative for BC/BCE)",
+      type: "string",
+      pattern: /^-[1-9][0-9]*|[1-9][0-9]*$/,
+      message: 'Year must be a non-zero positive or negative integer.',
+      required: true
+    }
   }
-]
+}
 
 // get user input
 prompt.start();
-prompt.get(prompt_attributes, function(err, result) {
+prompt.get(schema, function(err, result) {
   if (err) {
     console.log(err);
     return 1;
   } else {
-    var year = result.year;
+    var year = parseInt(result.year);
+    var year_text = "";
+    var url = ""; 
+    if (year > 0) {
+      year_text = "AD " + year;
+      url = "https://en.wikipedia.org/wiki/AD_" + year;
+    } else {
+      year_text = (year * -1) + " BC";
+      url = "https://en.wikipedia.org/wiki/" + (year * -1) + "_BC";
+    }
     // try to find the wikipedia page for this year
-    request("https://en.wikipedia.org/wiki/AD_" + year, function(error, response, body) {
+    request(url, function(error, response, body) {
       if (error) {
         console.log("Error: " + error);
         return 1;
@@ -44,7 +59,7 @@ prompt.get(prompt_attributes, function(err, result) {
           while (r2 == r1) {
             r2 = Math.floor(Math.random() * events.length);
           }
-          console.log("Two events from the year " + year + " (if not listed, date is unknown/NA):");
+          console.log("Two events from the year " + year_text + " (date not listed if unknown/NA):");
           console.log(events[r1]);
           console.log(events[r2]);
         }
