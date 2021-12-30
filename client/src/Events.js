a65b1a80f357443df05b3fbf95e993f6f108edb0
Katherine Yang <whykatherine@gmail.com>
2021-12-29 20:54:47 -0800

    fix: className

--- client/src/Events.js
+++ client/src/Events.js
@@ -1,10 +1,10 @@
 function Events(props) {
   return (
-    <div class="events">
+    <div className="events">
       <p>Two events from the year</p>
-      <p class="year">{(props.year > 0 ? props.year + " AD" : props.year * -1 + " BC")}</p>
+      <p className="year">{(props.year > 0 ? props.year + " AD" : props.year * -1 + " BC")}</p>
       <p>(date not listed if unknown/NA):</p>
-      <div class="pair">
+      <div className="pair">
         <div className="event">{props.events[0]}</div>
         <div className="event">{props.events[1]}</div>
       </div>
